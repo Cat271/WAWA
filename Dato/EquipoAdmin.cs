2c4736d89fc43e6a2e687d422a8253ea53fca177
Cat271 <cristianalexis271gmail.com>
2022-04-07 18:19:59 -0600

    Se agregaron las funciones a la capa de Dato.

--- Dato/EquipoAdmin.cs
+++ Dato/EquipoAdmin.cs
@@ -24,12 +24,47 @@ namespace WAWA.Dato
         /// <summary>
         /// Guardar un equipo en la BD
         /// </summary>
-        /// <param name="modelo"></param>
+        /// <param name="modelo">datos del equipo</param>
         public void Guardar(equipo modelo)
         {
             using (DWI_U4Entities contexto = new DWI_U4Entities())
             {
                 contexto.equipo.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Consultar
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public equipo Consultar(int id)
+        {
+            using (DWI_U4Entities contexto = new DWI_U4Entities())
+            {
+                return contexto.equipo.FirstOrDefault(v => v.idEquipo == id);
+            }
+        }
+
+        /// <summary>
+        /// Modifica los datos de un equipo
+        /// </summary>
+        /// <param name="modelo">datos de un equipo</param>
+        public void Modificar(equipo modelo)
+        {
+            using (DWI_U4Entities contexto = new DWI_U4Entities)
+            {
+                contexto.Entry(modelo).State = EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Eliminar(equipo modelo)
+        {
+            using (DWI_U4Entities contexto = new DWI_U4Entities())
+            {
+                contexto.Entry(modelo).State = EntityState.Deleted;
                 contexto.SaveChanges();
             }
         }
